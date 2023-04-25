7dcd64e723c01929c4d68bfda199f0c9b1d96a2e
Eyal Lapid <elpddev@pm.me>
2023-04-25 22:00:45 +0300

    working node server

--- apps/react-webcontainers/src/App.tsx
+++ apps/react-webcontainers/src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App() {
   const [container, setContainer] = useState<WebContainer | null>(null);
+  const [url, setUrl] = useState<string | undefined>(undefined);
 
   const onLoad = useEvent(async () => {
     const containerTemp = await WebContainer.boot();
@@ -21,7 +22,14 @@ function App() {
     (async () => {
       await container.mount(files);
       const packageJSON = await container.fs.readFile("package.json", "utf-8");
-      console.log(packageJSON);
+
+      const exitCode = await installDependencies(container);
+      if (exitCode !== 0) {
+        throw new Error("Installation failed");
+      }
+
+      const { url } = await startDevServer(container);
+      setUrl(url);
     })();
   }, [container]);
 
@@ -29,14 +37,14 @@ function App() {
     <div className="container">
       <div className="editor">
         <textarea
-          value="I am a textarea"
+          value={files["index.js"].file.contents}
           onChange={() => {
             /**  */
           }}
         ></textarea>
       </div>
       <div className="preview">
-        <iframe src="loading.html"></iframe>
+        <iframe src={url}></iframe>
       </div>
     </div>
   );
@@ -50,4 +58,39 @@ const useEvent = (callback: () => void) => {
     callbackRef.current = callback;
   });
   return useCallback(() => callbackRef.current(), []);
+};
+
+async function installDependencies(container: WebContainer) {
+  // Install dependencies
+  const installProcess = await container.spawn("npm", ["install"]);
+
+  installProcess.output.pipeTo(
+    new WritableStream({
+      write(data) {
+        console.log(data);
+      },
+    })
+  );
+
+  // Wait for install command to exit
+  return installProcess.exit;
+}
+
+async function startDevServer(container: WebContainer) {
+  // Run `npm run start` to start the Express app
+  await container.spawn("npm", ["run", "start"]);
+
+  // Wait for `server-ready` event
+  const [port, url] = await promisifyOn<[string, string]>(
+    "server-ready",
+    container
+  );
+
+  return { port, url };
+}
+
+const promisifyOn = (event: string, target: any) => {
+  return new Promise((resolve) => {
+    target.on(event, (...args: any[]) => resolve(args));
+  });
 };
